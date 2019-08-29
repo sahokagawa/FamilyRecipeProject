3a8d1fd072bed05b68a216f13f1220153aaf9a7a
sahokagawa <karaageyuko@gmail.com>
2019-08-29 16:50:51 +0800

    メンバー選択ページ、グループページにそれぞれ遷移できる

--- FamilyRecipeProject/Controllers/ViewController.swift
+++ FamilyRecipeProject/Controllers/ViewController.swift
@@ -44,12 +44,15 @@ extension ViewController: UICollectionViewDataSource, UICollectionViewDelegate {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath)
         
         let button = cell.viewWithTag(1) as! UIButton
-        
-        button.setImage(UIImage(named: "1"), for: .normal)
-        
         if indexPath.row == 0 {
             button.setImage(UIImage(named: "4"), for: .normal)
+            button.addTarget(self, action: #selector(goToChooseMember(_:)), for: UIControl.Event.touchUpInside)
+        } else {
+            button.addTarget(self, action: #selector(goToMyGroup(_:)), for: UIControl.Event.touchUpInside)
+            button.setImage(UIImage(named: "1"), for: .normal)
         }
+        
+        
         
         let label = cell.viewWithTag(2) as! UILabel
         label.text = "登録グループ"
@@ -58,9 +61,16 @@ extension ViewController: UICollectionViewDataSource, UICollectionViewDelegate {
             label.text = "追加"
         }
         
-        
         return cell
     }
     
+    @objc func goToChooseMember (_ sender: UIButton) {
+        performSegue(withIdentifier: "toChooseMember", sender: nil)
+    }
     
+    @objc func goToMyGroup (_ sender: UIButton) {
+        performSegue(withIdentifier: "toMyGroup", sender: nil)
+    }
+    
+
 }
